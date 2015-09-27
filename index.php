238d17159f253adf20e48b263cd22b3ceb3e4935
David NEMES <dokijo@sch.bme.hu>
2015-09-27 23:28:49 +0200

    index.php létrehozása

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+    ob_start();
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>BSS munkanapló</title>
+    </head>
+    
+    <body>
+        
+    </body>
+</html>
+
+<?php
+    ob_end_flush();
+?>
