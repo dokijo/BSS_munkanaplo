f590b6d6c27a8e8b89da682e7ab78d3d802742f5
David NEMES <dokijo@sch.bme.hu>
2015-09-27 23:45:16 +0200

    html head létrehozása és a body vázának elkészítése

--- index.php
+++ index.php
@@ -5,10 +5,26 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="sytle.css" type="text/css" media="screen" />
         <title>BSS munkanapló</title>
     </head>
     
     <body>
+        
+        <!-- Fejléc -->
+        <div id="fejlec_div">
+            <?php
+            
+            ?>
+        </div>
+        
+        <!-- Tartalom -->
+        <div id="tartalom_div">
+            <?php
+            
+            ?>
+        </div>
         
     </body>
 </html>
