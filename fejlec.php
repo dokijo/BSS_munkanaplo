81732f473f9d96ff81cbb06b00d308f34ddece41
David NEMES <dokijo@sch.bme.hu>
2015-09-29 08:57:06 +0200

    - fejlec.php létrehozása, indexben include-olása - GNU licence szöve hozzáadása a fájlokhoz

--- fejlec.php
+++ fejlec.php
@@ -17,15 +17,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-    session_start();
-    
-    function kiaz() {
-        if (isset($_SESSION['akt_user_ID'])) {
-            $akt_user_ID = $_SESSION['akt_user_ID'];
-        } else {
-            $akt_user_ID = -1;
-        }
-        return $akt_user_ID;
-    }
+
 
 ?>
